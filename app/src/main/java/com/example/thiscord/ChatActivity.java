1a98495f0377dfaa11211a64ac75c21e5056b131
안탄 <h7678036@naver.com>
2017-12-08 15:18:07 +0900

    어디까지했드라 ?

--- app/src/main/java/com/example/thiscord/ChatActivity.java
+++ app/src/main/java/com/example/thiscord/ChatActivity.java
@@ -3,6 +3,7 @@ package com.example.thiscord;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -11,7 +12,6 @@ import android.widget.EditText;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -21,12 +21,15 @@ public class ChatActivity extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
     private EditText input_chattext;
     private Button send_btn;
+    private Button invite_btn;
     private String user_id;
     private String nowingChat;
+    private String[] gmsg; // 받은 데이터 split으로 잘라서 넣어두는곳
+
 
     Socket socket;
-    private String ip = "10.0.2.2";   // 안드로이드 에뮬레이터에서는 localhost가 아니라 10.0.2.2로 접근!!
-    private int port =30000;
+    //private String ip = "10.0.2.2";   // 안드로이드 에뮬레이터에서는 localhost가 아니라 10.0.2.2로 접근!!
+    //private int port =30000;
     private Thread thread;
     DataInputStream dis;
     InputStream is;
@@ -36,14 +39,22 @@ public class ChatActivity extends AppCompatActivity {
     private String login_pwd;
     private String recv_msg; // 받은 메세지
 
+    public ChatActivity(){
+        this.socket = LoginActivity.socket;
+        this.dis = LoginActivity.dis;
+        this.is = LoginActivity.is;
+        this.dos = LoginActivity.dos;
+        this.os = LoginActivity.os;
+    }
+
 
     // onStart에서 서버를 미리 연결 해둠 (서버로 보낼 멀티쓰레드 구현 -> ??)
     @Override
     protected void onStart() {
         super.onStart();
-        sharedPreferences = getSharedPreferences("user", MODE_PRIVATE);
+        sharedPreferences = getSharedPreferences("user_id", MODE_PRIVATE);
         user_id = sharedPreferences.getString("userid", "0");
-
+        System.out.println("채팅액티비티 : 유저 - " + user_id);
     }
 
     @Override
@@ -58,7 +69,35 @@ public class ChatActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 System.out.println("유저아이디 : " + user_id);
-                connectServer();
+                //connectServer();
+            }
+        });
+
+        invite_btn = (Button)findViewById(R.id.invite_btn);
+        invite_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    System.out.println("챗룸클릭버튼1 : ");
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+                    Send_Msg(user_id + " " +
+
+                            "invite"); // 아이디랑를 보낸다
+                    System.out.println("챗룸클릭버튼2 : " );
+                    String msg = dis.readUTF();
+                    System.out.println("챗룸클릭버튼-서버에서 옴 : " + msg);
+                    gmsg = msg.split(" ");
+
+                    control_Msg(gmsg[0]);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    Log.e("client","connectServer() Error");
+                    return;
+                }
+
+
             }
         });
 
@@ -75,11 +114,7 @@ public class ChatActivity extends AppCompatActivity {
                 //Toast.makeText(getApplicationContext(),"Connect Server",Toast.LENGTH_LONG).show();
                 try {
 
-                    socket = new Socket(ip, port);
-                    is = socket.getInputStream();
-                    dis = new DataInputStream(is);
-                    os = socket.getOutputStream();
-                    dos = new DataOutputStream(os);
+
                     System.out.println("트라이는 들어옴");
                     Send_Msg(login_id+" "+nowingChat); // 현재 입력 되어 있는(에디트텍스트) 값을 보냄
 
@@ -113,7 +148,7 @@ public class ChatActivity extends AppCompatActivity {
 
     }
 
-    private void control_Msg(String str){ //
+    private void control_Msg(String str){
         if(str.equals("#OK")){  // login success
             System.out.println("login!");
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
@@ -126,40 +161,4 @@ public class ChatActivity extends AppCompatActivity {
             System.out.println("id/pw error!");
     }
 
-
-    class Recv_Msg implements Runnable{
-        @Override
-        public void run() {
-            Boolean flag=true;
-            while(flag) {
-                try{
-                    byte[] b = new byte[1024];
-                    dis.read(b);
-                    String msg = new String(b, "euc-kr");
-                    //  String msg = new String(b,0,b.length);
-                    String[] gmsg;
-                    gmsg = msg.split(" ");
-                    System.out.println(gmsg);
-                    recv_msg = gmsg[0];
-                    // recv_msg = msg;
-
-                    //mHandler.sendEmptyMessage(0);
-                }
-                catch(IOException e) {
-                    //e.printStackTrace();
-                    //status = false;
-                    try {
-                        os.close();
-                        is.close();
-                        dos.close();
-                        dis.close();
-                        socket.close();
-                        break;
-                    } catch (IOException e1) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    }
 }
--- app/src/main/java/com/example/thiscord/Main_roomlist_Fragment.java
+++ app/src/main/java/com/example/thiscord/Main_roomlist_Fragment.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +16,17 @@ import android.widget.Toast;
 
 import com.melnykov.fab.FloatingActionButton;
 
+import static com.example.thiscord.LoginActivity.dis;
+import static com.example.thiscord.LoginActivity.dos;
+
 
 public class Main_roomlist_Fragment extends Fragment {
     private FloatingActionButton floatingActionButton;
     private RecyclerView room_recyclerView ;
     private static final int request_code = 0;
     private Main_roomlistAdapter main_roomlistAdapter;
+
+    private FloatingActionButton refresh_btn;
 
     private String room_id; // 방이름
     private String login_user; // 접속한 유저 아이디
@@ -30,6 +37,7 @@ public class Main_roomlist_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle){
         View view = inflater.inflate(R.layout.fragment_main_roomlist_, container, false);
         room_recyclerView = (RecyclerView)view.findViewById(R.id.room_recycleview);
+        refresh_btn = (FloatingActionButton)view.findViewById(R.id.refresh_room_fab);
         return view;
     }
 
@@ -44,6 +52,31 @@ public class Main_roomlist_Fragment extends Fragment {
 
         sharedPreferences = this.getActivity().getSharedPreferences("user_id", Context.MODE_PRIVATE);
         login_user = sharedPreferences.getString("userid", "01012345678");
+
+        refresh_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    System.out.println("겟룸클릭버튼1 : ");
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+                    Send_Msg(login_user + " " + "getroom"); // 아이디랑를 보낸다
+                    System.out.println("겟룸클릭버튼2 : " );
+                    String msg = dis.readUTF();
+                    System.out.println("겟룸클릭버튼-서버에서 옴 : " + msg);
+                    gmsg = msg.split(" ");
+
+                    control_Msg(gmsg[0]);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    Log.e("client","connectServer() Error");
+                    return;
+                }
+            }
+        });
+        //connectServer();
+
 
 
         floatingActionButton = (FloatingActionButton)getActivity().findViewById(R.id.room_add_fab);
@@ -72,6 +105,34 @@ public class Main_roomlist_Fragment extends Fragment {
         if(!room_id.equals("cancle")){
           main_roomlistAdapter.addItem(new RoomContacts("1",room_id));
         }
+
+    }
+
+
+    public void Send_Msg(String msg){
+        Log.e("send","send success");
+        try{
+            //  dos.write(bb);
+            dos.writeUTF(msg);
+        }catch(Exception e){
+            e.printStackTrace();
+            Log.e("send","send failed");
+        }
+    }
+
+    private void control_Msg(String str){
+        if(str.equals("#getroom")){  // login success
+            System.out.println("길이 : " + gmsg.length);
+            for(int i = 1; i<gmsg.length; i++){
+                main_roomlistAdapter.addItem(new RoomContacts("1",gmsg[i]));
+            }
+            //main_roomlistAdapter.addItem(new RoomContacts("1",gmsg[1]));
+        }
+        else if(str.equals("#NOK")){ // pwd error
+            System.out.println("password error!");
+        }
+        else
+            System.out.println("id/pw error!");
     }
 
 
